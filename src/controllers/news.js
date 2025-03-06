e5ea7a7d437ca96cbca8b9a372b62ab41629a6a6
M SAI NITHIN <nithin6524@gmail.com>
2025-03-06 15:19:11 +0530

    modified business logic of news

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const { NEWS_API_KEY } = require("../utils/apiKeys");
 const getNews = async (req, res) => {
     try {
-        const {country, category}=req.query;
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources?country=${country}&category=${category}&apiKey=${NEWS_API_KEY}`);
+        const { category}=req.query;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?category=${category}&sortBy=popularity&apiKey=${NEWS_API_KEY}`);
         res.json(response.data);
     } catch (error) {    
         console.error("Error fetching news data:", error);
